aba0833fce0f90ed66b94f725366f006076f1118
E-U-P-H-O-R-I-A <133047304+E-U-P-H-O-R-I-A@users.noreply.github.com>
2024-11-08 14:49:31 +0200

    loaded the architecture

---
+++ Assets/CodeBase/Services/StaticDataService/IStaticDataService.cs
@@ -0,0 +1,13 @@
+﻿using CodeBase.Services.ServerConnectionService;
+using CodeBase.UI.PopUps.PolicyAcceptPopup;
+using Cysharp.Threading.Tasks;
+
+namespace CodeBase.Services.StaticDataService
+{
+    public interface IStaticDataService
+    {
+        UniTask InitializeAsync();
+        ServerConnectionConfig ServerConnectionConfig { get; }
+        PolicyAcceptPopupConfig GetPolicyAcceptPopupConfig(PolicyAcceptPopupTypes type);
+    }
+}
