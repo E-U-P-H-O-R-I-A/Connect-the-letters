15792abf1a39a13f4ff7abc7415e19afde58d9bf
E-U-P-H-O-R-I-A <133047304+E-U-P-H-O-R-I-A@users.noreply.github.com>
2024-11-14 15:09:04 +0200

    Fix private model parse

--- Assets/CodeBase/Scheme/Private/LevelPrivateScheme.cs
+++ Assets/CodeBase/Scheme/Private/LevelPrivateScheme.cs
@@ -2,45 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using CodeBase.Model.Private;
+using UnityEngine.Serialization;
 
 namespace CodeBase.Scheme.Private
 {
     [Serializable]
     public class LevelsPrivateScheme : IPrivateScheme
     {
-        private List<LevelPrivateScheme> _levels;
+        public List<LevelPrivateScheme> Levels;
         
         public LevelPrivateScheme GetLevel(int id)
         {
             CheckIsHaveScheme(id);
-            return _levels.First(scheme => scheme.Id == id);
+            return Levels.First(scheme => scheme.Id == id);
         }
 
         public void SetOpenLevel(int id)
         {
             CheckIsHaveScheme(id);
-            _levels.First(scheme => scheme.Id == id).SetOpened();
+            Levels.First(scheme => scheme.Id == id).SetOpened();
         }
 
         public void SetCompleteLevel(int id)
         {
             CheckIsHaveScheme(id);
-            _levels.First(scheme => scheme.Id == id).SetCompleted();
+            Levels.First(scheme => scheme.Id == id).SetCompleted();
         }
         
         private void CheckIsHaveScheme(int id)
         {
-            if (_levels.FirstOrDefault(scheme => scheme.Id == id) == null)
-                _levels.Add(new LevelPrivateScheme(id));
+            if (Levels.FirstOrDefault(scheme => scheme.Id == id) == null)
+                Levels.Add(new LevelPrivateScheme(id));
         }
     }
     
     [Serializable]
     public class LevelPrivateScheme
     {
-        public int Id { get; private set; }
-        public bool IsOpened { get; private set; }
-        public bool IsCompleted { get; private set; }
+        public int Id;
+        public bool IsOpened;
+        public bool IsCompleted;
         
         public LevelPrivateScheme(int id) => 
             Id = id;
